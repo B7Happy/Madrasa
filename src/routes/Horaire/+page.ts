4ac999b49e860498427d7eec6b9bd8d523c99f3d
saly.moubarak@gmail.com <saly.moubarak@gmail.com>
2024-05-20 22:42:49 +0200

    delete test folder

--- src/routes/Horaire/+page.ts
+++ src/routes/Horaire/+page.ts
@@ -3,7 +3,7 @@ import { dev } from '$app/environment';
 import { devApi, prodApi } from '$lib/Method/helper';
 
 export async function load({ fetch }) {
-    const response = await fetch(`${dev ? devApi : prodApi}Horaraire`);
+    const response = await fetch(`${dev ? devApi : prodApi}Horaire`);
     const horaires: Horaire[] = await response.json();
     return { horaires }
 }
